b0fc4cec4fe23e70cea1c9fba20e8e0f977342a5
Jesus-30 <64994313+Jesus-30@users.noreply.github.com>
2020-05-09 17:17:53 -0600

    Ahora el index tiene dos parrafos

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Mi Primera página </title>
 </head>
 <body>
-    
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores iste repellat, culpa inventore ullam est, officia quam delectus at iure ut dicta ipsam beatae eveniet blanditiis aperiam numquam necessitatibus nam.
+    </p>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consectetur quasi atque dolor ea consequuntur odio alias harum saepe adipisci voluptas eum praesentium qui, nobis quidem eos ipsum, recusandae blanditiis.
+    </p>
 </body>
 </html>
